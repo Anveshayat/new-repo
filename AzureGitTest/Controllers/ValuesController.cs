9b02abab00bbae51601c812c475990a225123473
Anvesh <anvesh.v@nichebit.com>
2026-03-27 17:41:39 +0530

    Added new controller

---
+++ AzureGitTest/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureGitTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
