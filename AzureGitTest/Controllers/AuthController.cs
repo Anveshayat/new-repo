236697f0aab5123188502c4ded74687b3dd3554d
Anvesh <anvesh.v@nichebit.com>
2026-04-01 12:21:30 +0530

    git push

--- AzureGitTest/Controllers/AuthController.cs
+++ AzureGitTest/Controllers/AuthController.cs
@@ -68,7 +68,7 @@ namespace AzureGitTest.Controllers
                 }
                 else
                 {
-                    return Unauthorized("Invalid usernames or passwords");
+                    return Unauthorized("Invalid usernamess or passwords");
                 }
             }
         }
